bdee288b0df1667b2ccc464bc66f623f80335eef
Daviyisu <davidjrvt@hotmail.com>
2021-03-23 12:41:56 +0100

    Añadida caja de comentarios y dos comentarios de ejemplo

---
+++ P2/scripts/evento.js
@@ -0,0 +1,8 @@
+function esconderComentarios() {
+    var x = document.getElementById("caja_comentarios");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
