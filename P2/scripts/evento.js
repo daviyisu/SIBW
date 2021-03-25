d04afcb2e6323f6381bbef01d289590ee154d408
Daviyisu <davidjrvt@hotmail.com>
2021-03-25 10:40:10 +0100

    se puede postear un comentario pero no tiene css

--- P2/scripts/evento.js
+++ P2/scripts/evento.js
@@ -6,3 +6,15 @@ function esconderComentarios() {
       x.style.display = "none";
     }
   } 
+
+  function postearComentario(){
+    var f = new Date();
+    var comentario = document.getElementById("fname").value;
+    comentario += " ";
+    comentario += f.getDate() + "-"+ (f.getMonth()+1)+ "-" +f.getFullYear();
+    comentario += " ";
+    comentario += f.getHours() + ":" + f.getMinutes() + ":"
+    comentario += " ";
+    comentario += document.getElementById("texto").value;
+    return comentario;
+  }
